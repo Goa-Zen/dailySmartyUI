eea540c07fee49e145ca8f0f716eda31f2be7ee6
Goa-Zen <goazengoazen.cr@gmail.com>
2024-09-10 18:24:45 +0200

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>My tttle</h1>
+        <h2>My subtitle</h2>
+      </div>
+    );
+  }
+}
